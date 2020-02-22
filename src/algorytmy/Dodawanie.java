86458a4b3312404cf0a359f7fe79654d722bca94
jarpar <partyka.jaroslaw@gmail.com>
2020-02-22 14:14:16 +0100

    refactored

--- src/algorytmy/Dodawanie.java
+++ src/algorytmy/Dodawanie.java
@@ -2,12 +2,8 @@ package algorytmy;
 
 public class Dodawanie {
     public static void main(String[] args) {
-        System.out.println(dodaj(4));
         System.out.println(dodajRek(4));
-    }
-
-    private static int dodaj(int liczba) {
-        return 0;
+        System.out.println(silniaRek(4));
     }
 
     private static int dodajRek(int liczba) {
@@ -16,5 +12,13 @@ public class Dodawanie {
             return 0;
         }
         return liczba + dodajRek(liczba - 1);
+    }
+
+    private static int silniaRek(int liczba) {
+
+        if (liczba == 0) {
+            return 1;
+        }
+        return liczba * silniaRek(liczba - 1);
     }
 }
