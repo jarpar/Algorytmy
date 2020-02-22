3191a7080d54e3092c1f1c88c41349514edbc60d
jarpar <partyka.jaroslaw@gmail.com>
2020-02-22 14:46:04 +0100

    refactored

--- src/algorytmy/Fibonacci.java
+++ src/algorytmy/Fibonacci.java
@@ -2,7 +2,10 @@ package algorytmy;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(mathFibo(10));
+        for (int i = 0; i <= 19; i++) {
+            System.out.println(mathFibo(i));
+        }
+
     }
 
     int n;
