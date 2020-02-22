19604c6591014f7804ebd58051ebd4e613158bf1
jarpar <partyka.jaroslaw@gmail.com>
2020-02-22 18:46:41 +0100

    refactored

--- src/algorytmy/homework/TabReverseRecursion.java
+++ src/algorytmy/homework/TabReverseRecursion.java
@@ -10,19 +10,33 @@ public class TabReverseRecursion {
 
         }
 
-        reverse(tab);
-
         System.out.println("\nReversed:");
 
-        for (int n : tab
+        for (int n : reverse(tab)
         ) {
             System.out.print(n + " ");
 
         }
+
     }
 
+    private static int n = 0;
+    private static int[] revTab = new int[tab.length];
+
     static int[] reverse(int[] tab) {
-        int[] revTab = tab.clone();
-        return revTab;
+        if (n == tab.length) {
+            return revTab;
+        }
+        revTab[n] = tab[tab.length - 1 - n];
+        n++;
+        return reverse(tab);
     }
 }
+
+/*
+/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java
+1 2 3 4 5
+Reversed:
+5 4 3 2 1
+Process finished with exit code 0
+ */
