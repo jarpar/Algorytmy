5e7b8c897966c160111dd42894bd59301e6879b0
jarpar <Mamotoja88>
2020-02-23 19:34:17 +0100

    refactored

--- src/algorytmy/homework/Factorial.java
+++ src/algorytmy/homework/Factorial.java
@@ -17,22 +17,22 @@ public class Factorial {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Factorial " + i + "!=" + factorial(i));
+            System.out.println("Factorial " + i + "! = " + factorial(i));
         }
     }
 }
 /*
-/usr/lib/jvm/default-java/bin/java
-Factorial 0!=1
-Factorial 1!=1
-Factorial 2!=2
-Factorial 3!=6
-Factorial 4!=24
-Factorial 5!=120
-Factorial 6!=720
-Factorial 7!=5040
-Factorial 8!=40320
-Factorial 9!=362880
+/usr/lib/jvm/default-java/bin/java -javaagent:/snap/intellij-idea-community/208/lib/idea_rt.jar=37107:/snap/intellij-idea-community/208/bin -Dfile.encoding=UTF-8 -classpath /home/user/IdeaProjects/Algorytmy/out/production/Algorytmy algorytmy.homework.Factorial
+Factorial 0! = 1
+Factorial 1! = 1
+Factorial 2! = 2
+Factorial 3! = 6
+Factorial 4! = 24
+Factorial 5! = 120
+Factorial 6! = 720
+Factorial 7! = 5040
+Factorial 8! = 40320
+Factorial 9! = 362880
 
 Process finished with exit code 0
  */
