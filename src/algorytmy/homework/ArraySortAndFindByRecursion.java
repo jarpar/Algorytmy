6bbd0443052048704538d454d6fd6acd9842f7c1
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-23 11:00:53 +0100

    refactored

--- src/algorytmy/homework/ArraySortAndFindByRecursion.java
+++ src/algorytmy/homework/ArraySortAndFindByRecursion.java
@@ -3,7 +3,6 @@ package algorytmy.homework;
 import java.util.Arrays;
 
 public class ArraySortAndFindByRecursion {
-    //private static int[] tab = new int[]{4, 6, 2, 5, 8, 9, 1, 3, 7};
 
     public static void main(String[] args) {
         //int[] tab = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
