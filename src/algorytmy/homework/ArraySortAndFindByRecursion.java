f80b72fe3493a8bfb04a57ac5f2c1ef3286de45a
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-23 09:26:39 +0100

    refactored

--- src/algorytmy/homework/ArraySortAndFindByRecursion.java
+++ src/algorytmy/homework/ArraySortAndFindByRecursion.java
@@ -1,28 +1,28 @@
-////not finnished yet!
-//package algorytmy.homework;
-//
-//import java.util.Arrays;
-//
-//public class ArraySortAndFindByRecursion {
-//    private static int[] tab = new int[]{4, 6, 2, 5, 8, 9, 1, 3, 7};
-//
-//    public static void main(String[] args) {
-//        Arrays.sort(tab);
-//    }
-//
-//    static int sortTab(int[] tab, int find) {
-//        int tempFind = 0;
-//        int place = 0;
-//        if (tempFind == find) {
-//            return place;
-//        }
-//        if (find == tab[tab.length / 2]) {
-//            return tab.length / 2;
-//        } else if (find < tab[tab.length / 2]) {
-//            return place = tab.length / 2;
-//        } else if (find > tab[tab.length / 2]) {
-//            return place = tab.length / 2;
-//        }
-//        return sortTab(tab, find);
-//    }
-//}
+//not finnished yet!
+package algorytmy.homework;
+
+import java.util.Arrays;
+
+public class ArraySortAndFindByRecursion {
+    private static int[] tab = new int[]{4, 6, 2, 5, 8, 9, 1, 3, 7};
+
+    public static void main(String[] args) {
+        Arrays.sort(tab);
+    }
+
+    static int sortTab(int[] tab, int find) {
+        int tempFind = 0;
+        int place = 0;
+        if (tempFind == find) {
+            return place;
+        }
+        if (find == tab[tab.length / 2]) {
+            return tab.length / 2;
+        } else if (find < tab[tab.length / 2]) {
+            return place = tab.length / 2;
+        } else if (find > tab[tab.length / 2]) {
+            return place = tab.length / 2;
+        }
+        return sortTab(tab, find);
+    }
+}
---
+++ src/tablice/Tablice.java
@@ -0,0 +1,18 @@
+package tablice;
+
+public class Tablice {
+    public static void main(String[] args) {
+
+        revVals(theVals);
+    }
+
+    private static void revVals(float[] theVals) {
+        for (int i = 0; i < theVals.length; i++) {
+            System.out.print((revVals[i] = theVals[theVals.length - i - 1]) + " ");
+        }
+    }
+
+    private static float[] theVals = {10.0f, 20.0f, 30.0f};
+    private static float[] revVals = new float[theVals.length];
+
+}
