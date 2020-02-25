2ea4ea5d54b82a3716fec6aeae2d7c5319ca514e
jarpar <partyka.jaroslaw@gmail.com>
2020-02-25 20:06:58 +0100

    created whole program while nothing else to do, cheers!

--- src/homework/HanoiByRecursive.java
+++ src/homework/HanoiByRecursive.java
@@ -3,9 +3,10 @@ package homework;
 public class HanoiByRecursive {
     public void move(int n, char start, char middle, char end) {
         if (n == 1) {
-            System.out.println("char 1 from " + start + "to " + end);
+            System.out.println("Element 1 from " + start + "to " + end);
             return;
         }
         move(n - 1, start, middle, end);
+        System.out.println("Element " + n + " from " + start + " to " + middle);
     }
 }
