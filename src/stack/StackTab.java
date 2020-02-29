ef8cc9ca5f33e63c569a1dde9c22648a5ba8e956
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-29 12:34:45 +0100

    refactored

--- src/stack/StackTab.java
+++ src/stack/StackTab.java
@@ -32,8 +32,8 @@ public class StackTab {
     }
 
     public int search(int value) {
-        for (int i = 0; i < stack.length; i++) {
-            if (i == value) {
+        for (int i = 0; i < index; i++) {
+            if (stack[i] == value) {
                 return i;
             }
         }
@@ -68,10 +68,15 @@ class test {
         StackTab stackTab = new StackTab(10);
         stackTab.push(1);
         stackTab.push(2);
-        stackTab.push(3);
         stackTab.push(4);
+        stackTab.push(3);
         stackTab.push(5);
         stackTab.print();
+        System.out.println();
+        System.out.println("searched 3 is at index:");
+        System.out.println(stackTab.search(3));
+        System.out.println("searched 4 is at index:");
+        System.out.println(stackTab.search(4));
         System.out.println();
         System.out.println(stackTab.peek());
         System.out.println(stackTab.pop());
