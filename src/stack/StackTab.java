5924e3f096179e3da52902aa4c7ca48b39c30e99
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-29 11:53:41 +0100

    refactored

--- src/stack/StackTab.java
+++ src/stack/StackTab.java
@@ -1,8 +1,5 @@
 package stack;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class StackTab {
     int[] stack;
     int size;
@@ -13,13 +10,19 @@ public class StackTab {
     }
 
     public int pop() {//zwraca i usuwa
-        int temp = stack[index];
-        stack[index] = stack[index + 1];
-        return temp;
+        int temp;
+        for (int i = 0; i < stack.length; i++) {
+            if (i != index) {
+                temp = stack[i];
+
+            }
+        }
+        index--;
+        return stack[index];
     }
 
     public int peek() {//tylko zwraca
-        return stack[index];
+        return stack[index - 1];
     }
 
     public int search(int value) {
@@ -31,9 +34,13 @@ public class StackTab {
         return -1;
     }
 
-    public int push(int value) {//dokłada na stos
+    public void push(int value) throws Exception {//dokłada na stos
+        if (index >= stack.length) {
+            throw new Exception("can't add next value");
+        }
         stack[index] = value;
-        return stack[index];
+        index++;
+        //return value;
     }
 
     public int getSize() {
@@ -41,5 +48,23 @@ public class StackTab {
         for (int i : stack)
             tempSize++;
         return tempSize;
+    }
+}
+
+class test {
+    public static void main(String[] args) throws Exception {
+        StackTab stackTab = new StackTab(10);
+        stackTab.push(1);
+        stackTab.push(2);
+        stackTab.push(3);
+        stackTab.push(4);
+        stackTab.push(5);
+        System.out.println(stackTab.peek());
+        System.out.println(stackTab.pop());
+        System.out.println(stackTab.peek());
+        stackTab.push(6);
+        System.out.println(stackTab.peek());
+        System.out.println(stackTab.pop());
+        System.out.println(stackTab.peek());
     }
 }
