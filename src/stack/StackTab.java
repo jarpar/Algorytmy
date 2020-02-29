b6446ebcf4e2a8a21b01e35f546c176dd4baface
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-29 11:11:28 +0100

    refactored

--- src/stack/StackTab.java
+++ src/stack/StackTab.java
@@ -1,22 +1,45 @@
 package stack;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class StackTab {
     int[] stack;
     int size;
+    int index = 0;
+
+    public StackTab(int size) {
+        stack = new int[size];
+    }
 
     public int pop() {//zwraca i usuwa
-        return -1;
+        int temp = stack[index];
+        stack[index] = stack[index + 1];
+        return temp;
     }
 
     public int peek() {//tylko zwraca
-        return -1;
+        return stack[index];
     }
 
     public int search(int value) {
+        for (int i = 0; i < stack.length; i++) {
+            if (i == value) {
+                return i;
+            }
+        }
         return -1;
     }
 
     public int push(int value) {//dokłada na stos
-        return -1;
+        stack[index] = value;
+        return stack[index];
+    }
+
+    public int getSize() {
+        int tempSize = 0;
+        for (int i : stack)
+            tempSize++;
+        return tempSize;
     }
 }
