55c386853bd2264c37c1c541d127210a6199eb68
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-29 10:46:02 +0100

    refactored

--- src/stack/StackTab.java
+++ src/stack/StackTab.java
@@ -1,5 +1,22 @@
 package stack;
 
 public class StackTab {
+    int[] stack;
+    int size;
 
+    public int pop() {//zwraca i usuwa
+        return -1;
+    }
+
+    public int peek() {//tylko zwraca
+        return -1;
+    }
+
+    public int search(int value) {
+        return -1;
+    }
+
+    public int push(int value) {//dokłada na stos
+        return -1;
+    }
 }
